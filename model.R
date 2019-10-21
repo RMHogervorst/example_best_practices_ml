cd14929327b99a7c379eca896cdf7b471ccfd868
Roel M. Hogervorst <hogervorst.rm@gmail.com>
2019-10-21 08:30:49 +0200

    Add description of whole process

--- model.R
+++ model.R
@@ -1,21 +1,28 @@
-library(mlbench)
-data("Shuttle")
-library(tidymodels)
-# recipes for feature eng
-data_set <- Shuttle
-# predict HHV, or use credit_data to predict Status
-# or create dataset of webtraffic and what they buy? Churn or something
-# or shuttle
-# Make better real life example by dropping random 
+############### An ML example #####################
+# In this example we will read in data
+# split the data into train and test
+# train a model on the train data data
+# validate against the test data
+# and return some metrics.
 
-# NASA Shuttle database deals with the positioning of radiators169in the Space
+
+## Load the packages  ----
+library(mlbench)
+library(tidymodels)
+
+# load the data -----
+data("Shuttle")
+data_set <- Shuttle
+
+
+# NASA Shuttle database deals with the positioning of radiators in the Space
 # Shuttle. In the original, it consists of 43500 training objects and 14500 test
-# objects. Each170instance is described by nine continuous attributes and is
+# objects. Each instance is described by nine continuous attributes and is
 # assigned to one of seven classes with the171frequencies
 
 # this should probably be separate because no one would introduce missings 
 # during the training on purpose.
-# add missings
+# add missings ----
 set.seed(22345)
 len <- nrow(data_set)
 data_set$V1[sample(x = 1:len,size = 10)] <- NA
@@ -24,19 +31,26 @@ data_set$V3[sample(x = 1:len,size = 10)] <- NA
 data_set$V7[sample(x = 1:len,size = 10)] <- NA
 data_set$V9[sample(x = 1:len,size = 10)] <- NA
 
-# split
+# split the data into train and test data ----
+# This is not a time series so we don't care where the data is split.
+set.seed(3456) # setting seed here, so we can run this part indepentenly
 train_test_samples <- rsample::initial_split(data_set,prop = 3/4)
 train_data <- training(train_test_samples)
 test_data <- testing(train_test_samples)
+
+# optionally remove data we no longer need.
 rm(data_set, Shuttle)
-# target Class
+
+# Feature Engineering -----
+# target Class, use everything else to predict
+# These next steps are only the procedure. We do not yet actually compute and 
+#  execute on the steps.
 rec <- recipe(train_data, Class~.) %>% 
-# Feature engineering
   # - deal with missings
     step_knnimpute(V1, V2, V3, V7, V9, 
                    neighbors = 6,
                    id = "Missing imputation") %>% 
-    step_mutate(
+    step_mutate( # some manual reworking because I thought these were useful
       V2sign = V2 <0,
       V2 = log1p(abs(V2)),
       V5sign = V5 <0,
@@ -47,46 +61,47 @@ rec <- recipe(train_data, Class~.) %>%
       V7 = sqrt(abs(V7)),
       V9sign = V9 < 0,
       V9 = log1p(abs(V9)),
-      #Class = fct_collapse(Class, "other"=c("Bpv.Open", "Bpv.Close")),
       id = "Sign and logging"
     ) %>% 
-# - scale
+  # - transform yeo_something
     step_YeoJohnson(all_numeric(),id = "transformation of vars") %>% 
-# - transform yeo_something
+  # - scale and center
     step_center(all_numeric(), id = "centering vars") %>% 
     step_scale(all_numeric(), id = "scaling vars")
-# - partial least squares?
+
+# Learn what the values should be, from the training data
 rec_prepped <- prep(rec, training = train_data)
-# Train model
+
+# Execute the Featuer engineering on the training and test data
+# this might seem silly here:
+# * we first tell it what to do in global
+# * then learn what to do specifically
+# * and only then execute 
+# 
+# And this is slightly overkill for here, But when you run multiple models, 
+# or use resampling it makes a lot of sense to first specify your steps, 
+# and execute them in seperate steps. 
+# 
+# Run feature Engineering on Test and training data
 train_data_FE <- bake(rec_prepped, train_data)
 test_data_FE <- bake(rec_prepped, test_data)
-# cross validate and use resample to estimate best value
-# predict on test set
-# 
 
-# it was actually extremely great performance by default.
-# but for the sake of the argument let's find the best parameters for the 
-# model
+
+
+# Run a model on the training data. -----
 shuttle_ranger <- 
   rand_forest(trees = 100, mode = "classification") %>%
   set_engine("ranger") %>% 
   fit(Class~., train_data_FE)
 
-#%>% 
-#  fit(Class~., train_data_FE)
 
-# bst_grid <- grid_random(
-#   trees() %>% range_set( c( 70,  200)), 
-#   mtry() %>% range_set(c( 2,  9)), 
-#   size = 10, original = TRUE
-# )
-
-
+### Metrics -----
 prediction <- predict(shuttle_ranger, test_data_FE)
 test_data_FE %>% 
   bind_cols(prediction) %>% 
   metrics(truth = Class, estimate = .pred_class)
 
+# A slight exploration of performance per class
 prediction2 <- predict(shuttle_ranger, test_data_FE,type = "prob")
 
 probs <- 
